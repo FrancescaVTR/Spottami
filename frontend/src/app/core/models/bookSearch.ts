26a913239d9455d01f50f74d4ab6cc68eb78e80a
FrancescaVTR <francesca.vetru@gmail.com>
2022-07-12 12:10:27 +0200

    Login

--- frontend/src/app/core/models/bookSearch.ts
+++ frontend/src/app/core/models/bookSearch.ts
@@ -1,9 +1,6 @@
-export class RoomBooking {
+export class BookSearch {
     constructor (
-        public user_id: number,
         public room_id: number,
         public booking_date: string,  // DATE => YYYY-MM-DD hh:mm:ss
-        public start_time: string,
-        public end_time: string
     ) { }
 }
